b8abb40909ac4cf27f5a0b0946fec8031b80ce83
Csaru2 <cesar27andrade@icloud.com>
2026-04-23 13:51:21 -0300

    Add files via upload

--- financas/src/supabaseClient.js
+++ financas/src/supabaseClient.js
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-
 const SUPABASE_URL = 'https://evzoyywlimskzznzchxt.supabase.co'
 const SUPABASE_KEY = 'sb_publishable_lRZZHoH9MnGuNLu0V6rV_A_h97v_22D'
-
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
